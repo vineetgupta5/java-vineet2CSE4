720d806a111fbabeb2f2f24b7f8044a9b26a6a65
vineetgupta5 <vineetgupta5665@gmail.com>
2025-09-13 11:26:55 +0530

    Create Cons1.java

---
+++ Cons1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ public class Geek {
+    Geek()
+    {
+       
+        System.out.println("constructor is called");
+
+    }
+    public static void main(String[] args) {
+         Geek geek = new Geek();
+    }
+   
+}
