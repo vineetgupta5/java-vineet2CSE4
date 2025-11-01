d73ca248114c01e192ccd48f202c95eba2c1a8e0
vineetgupta5 <vineetgupta5665@gmail.com>
2025-11-01 15:17:43 +0530

    Create Casechange.java

---
+++ Casechange.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Geeks {
+    public static void main(String[] args) {
+       String s = "Vineet";
+       System.out.println(s.toUpperCase()); 
+       System.out.println(s.toLowerCase());
+    }
+}
